eba771d7eef30edb978f2b14ee38820e6b2318e0
joaoragazzo <joao_ragazzo@hotmail.com>
2025-07-27 17:53:38 -0300

    fix: add prefix to the API link

--- front-end/src/config/axiosClient.ts
+++ front-end/src/config/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://no-admin-abuse.joaoragazzo.dev/",
+    baseURL: "https://no-admin-abuse.joaoragazzo.dev/api/v1/",
     timeout: 10000,
     withCredentials: true,
     headers: {
