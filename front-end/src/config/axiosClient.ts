48e17a3b7f7475b5e918efca8b47790ae71d8acc
joaoragazzo <joao_ragazzo@hotmail.com>
2025-07-31 19:16:04 -0300

    fix: fixing the development api URL on axios

--- front-end/src/config/axiosClient.ts
+++ front-end/src/config/axiosClient.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/",
-    // baseURL: "https://no-admin-abuse.joaoragazzo.dev/api/v1/",
+    // baseURL: "http://localhost:8080/",
+    baseURL: "https://no-admin-abuse.joaoragazzo.dev/api/v1/",
     timeout: 10000,
     withCredentials: true,
     headers: {
