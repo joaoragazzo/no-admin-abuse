df33b9130bbb76eb344586e507c1ba233b928565
joaoragazzo <joao_ragazzo@hotmail.com>
2025-08-07 17:10:27 -0300

    fix: fixing the header width for the page

--- front-end/src/pages/Network/NetworkHome.tsx
+++ front-end/src/pages/Network/NetworkHome.tsx
@@ -39,50 +39,53 @@ export const NetworkHome: React.FC = () => {
 
     return (
     <>   
-        <div className="px-10 md:px-20 xl:px-50 w-full bg-gray-950 flex flex-row gap-5 py-20">
-            <div className="border-2 text-blue-800 border-blue-800 bg-blue-980 w-fit h-fit p-8 rounded-md">
-                <FaSkullCrossbones size={33}/>
-            </div>
-            <div className="flex flex-col gap-2.5 w-full">
-                <div className="flex flex-row justify-between items-center">
-                    <h2 className="font-extrabold text-3xl">
-                        {networkDetails?.name}
-                    </h2>
-                    <h2 
-                        onClick={() => {navigate(-1)}}
-                        className="font-bold text-1xl flex items-center gap-2 cursor-pointer text-gray-200 hover:text-white">
-                        Voltar <FaArrowRight />
-                    </h2>
+        {/* Header */}
+        <div className="bg-gray-950 py-20 flex flex-row justify-center">
+            <div className="flex max-w-400 gap-5 flex-grow px-10 md:px-20 xl:px-50">
+                <div className="border-2 text-blue-800 border-blue-800 bg-blue-980 w-fit h-fit p-8 rounded-md">
+                    <FaSkullCrossbones size={33}/>
                 </div>
-                
-                
-                <div className="flex flex-row gap-4 text-sm">
-                    <div className="flex flex-row gap-3">
-                        <Rating rating={4}/>
-                        (342 avaliações)
-                    </div>
-                    •
-                    <div>
-                        {networkDetails.servers.length} servidores ativos
-                    </div>
-                </div>
-                <div className="flex flex-row items-center gap-6">
-                    <div className="bg-green-600 px-2.5 py-1 w-fit rounded-full text-xs font-semibold flex items-center gap-1.5">
-                        <MdVerified /> Servidor verificado
-                    </div>
-                    <div className="flex flex-row items-center gap-1.5 text-xs">
-                        <div className="p-1 bg-green-600 rounded-full w-fit h-fit">
-                        </div>
-                        <div className="text-green-600 flex items-center">
-                            {onlinePlayersCount}/{maxPlayersCount} jogadores online
-                        </div>
+                <div className="flex flex-col gap-2.5 w-full">
+                    <div className="flex flex-row justify-between items-center">
+                        <h2 className="font-extrabold text-3xl">
+                            {networkDetails?.name}
+                        </h2>
+                        <h2 
+                            onClick={() => {navigate(-1)}}
+                            className="font-bold text-1xl flex items-center gap-2 cursor-pointer text-gray-200 hover:text-white">
+                            Voltar <FaArrowRight />
+                        </h2>
                     </div>
                     
+                    
+                    <div className="flex flex-row gap-4 text-sm">
+                        <div className="flex flex-row gap-3">
+                            <Rating rating={4}/>
+                            (342 avaliações)
+                        </div>
+                        •
+                        <div>
+                            {networkDetails.servers.length} servidores ativos
+                        </div>
+                    </div>
+                    <div className="flex flex-row items-center gap-6">
+                        <div className="bg-green-600 px-2.5 py-1 w-fit rounded-full text-xs font-semibold flex items-center gap-1.5">
+                            <MdVerified /> Servidor verificado
+                        </div>
+                        <div className="flex flex-row items-center gap-1.5 text-xs">
+                            <div className="p-1 bg-green-600 rounded-full w-fit h-fit">
+                            </div>
+                            <div className="text-green-600 flex items-center">
+                                {onlinePlayersCount}/{maxPlayersCount} jogadores online
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         </div>
         
-        
+        {/* Content */}
         <div className="w-full flex justify-center">
             <div className="flex flex-row gap-10 px-10 md:px-20 xl:px-50 py-10 max-w-400">
                 <div className="flex flex-col basis-2/3 gap-8">
