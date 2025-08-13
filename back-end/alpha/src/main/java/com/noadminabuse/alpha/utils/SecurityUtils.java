1384fe7bb71548e1ccf7a76291db5c58a55f6e82
joaoragazzo <joao_ragazzo@hotmail.com>
2025-08-13 18:48:41 -0300

    feat: add a error message to unauthenticated requests

--- back-end/alpha/src/main/java/com/noadminabuse/alpha/utils/SecurityUtils.java
+++ back-end/alpha/src/main/java/com/noadminabuse/alpha/utils/SecurityUtils.java
@@ -2,15 +2,23 @@ package com.noadminabuse.alpha.utils;
 
 import java.util.UUID;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.noadminabuse.alpha.errors.Unauthorized;
+import com.noadminabuse.alpha.errors.enums.AuthErrorMessage;
 
 public class SecurityUtils {
     
     public static UUID getCurrentUserId() {
-        String uuidString = SecurityContextHolder
-            .getContext()
-            .getAuthentication()
-            .getName();
-        return UUID.fromString(uuidString);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    
+        if (auth != null && auth.isAuthenticated() && !(auth instanceof AnonymousAuthenticationToken)) {
+            String uuidString = auth.getName();
+            return UUID.fromString(uuidString);
+        } else {
+            throw new Unauthorized(AuthErrorMessage.UNAUTHENTICATED_REQUEST);
+        }
     }
 }
