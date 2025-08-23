c8347b92ed31e277caf12e67ede823da08b0a3ec
joaoragazzo <joao_ragazzo@hotmail.com>
2025-08-23 16:39:34 -0300

    feat: improving menu

--- front-end/src/components/layout/Header.tsx
+++ front-end/src/components/layout/Header.tsx
@@ -1,9 +1,9 @@
 import { FaBars, FaGamepad } from "react-icons/fa"
 import { LoginSteamButton } from "../button/LoginSteamButton"
 import { FaShield } from "react-icons/fa6"
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Brand } from "../Brand";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "@/contexts/AuthContext";
 import Popup from "reactjs-popup";
 import { LoginPopup } from "./LoginPopup";
@@ -13,12 +13,44 @@ import { SafeLoginPopup } from "../popups/SafeLoginPopup";
 export const Header: React.FC = () => {
     const [menuOpen, setMenuOpen] = useState<boolean>(false);
     const [safeLoginPopup, setSafeLoginPopup] = useState<boolean>(false);
+    const navigate = useNavigate();
     const { user, isAuthenticated } = useAuth();
     
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const hamburgerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+          if (
+            menuOpen &&
+            dropdownRef.current &&
+            hamburgerRef.current &&
+            !dropdownRef.current.contains(event.target as Node) &&
+            !hamburgerRef.current.contains(event.target as Node)
+          ) {
+            setMenuOpen(false);
+          }
+        };
+    
+        document.addEventListener('mousedown', handleClickOutside);
+        
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+      }, [menuOpen]);
+
 
     const items = [
         {
-            content: <Link to="/games" onClick={() => {setMenuOpen(false)}} className="flex flex-row items-center gap-3"><FaGamepad />Jogos</Link> 
+            content: 
+                <Link 
+                    to="/games" 
+                    onClick={() => {setMenuOpen(false)}} 
+                    className="flex flex-row items-center gap-3"
+                >
+                    <FaGamepad />Jogos
+                </Link>,
+            link: "/games"
         },
         {
             content: 
@@ -35,9 +67,15 @@ export const Header: React.FC = () => {
                     <FaShield onClick={() => {setSafeLoginPopup(true)}} className="text-black hover:bg-white bg-gray-100 hover:scale-115 transition-all w-6 h-6 p-1.5 rounded-md"/>
                 </div>
             }
-            </>
+            </>,
+            link: "/games"
         }
     ]
+
+    const handleMenuClick = (path: string) => {
+        setMenuOpen(false);
+        navigate(path);
+    }
     
     return (
     <>
@@ -50,14 +88,25 @@ export const Header: React.FC = () => {
                     </div>
                 ))}
             </div>
-            <div className="lg:hidden flex cursor-pointer" onClick={() => {setMenuOpen(!menuOpen)}}>
+            <div 
+                ref={hamburgerRef}
+                className="lg:hidden flex cursor-pointer" 
+                onClick={() => {setMenuOpen(!menuOpen)}}
+            >
                 <FaBars size={24}/>
             </div>
         </header>
 
-        <div className={`${menuOpen ? 'flex' : 'hidden'} mt-18 lg:hidden flex-col absolute w-full bg-blue-980`}>
+        <div
+            ref={dropdownRef} 
+            className={`${menuOpen ? 'flex' : 'hidden'} mt-18 lg:hidden flex-col absolute w-full bg-blue-980`}
+        >
             {items.map((content, key) => (
-                <div key={key} className="flex flex-row items-center gap-3 w-full border-b-1 border-white/10 py-4 px-10 hover:bg-blue-950/50 hover:text-blue-400 cursor-pointer">
+                <div
+                    onClick={() => {handleMenuClick(content.link)}}
+                    key={key} 
+                    className="flex flex-row items-center gap-3 w-full border-b-1 border-white/10 py-4 px-10 hover:bg-blue-950/50 hover:text-blue-400 cursor-pointer"
+                >
                     {content.content}
                 </div>
             ))}
