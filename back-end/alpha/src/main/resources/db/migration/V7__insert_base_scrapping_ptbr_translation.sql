a4234afa8c9cc8ccb98c6e142e5ee7da4e27109e
joaoragazzo <joao_ragazzo@hotmail.com>
2025-10-14 21:02:48 -0300

    feat: add basic translation for mocked scrapping data

---
+++ back-end/alpha/src/main/resources/db/migration/V7__insert_base_scrapping_ptbr_translation.sql
@@ -0,0 +1,24 @@
+-- V7__insert_base_scrapping_ptbr_translation.sql
+-- Insert the base translation for base tags
+
+INSERT INTO translation(id, lang, t_key, t_value, namespace) VALUES
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA884"), "pt", "dayz.vanillapp", "Vanilla++", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA883"), "pt", "dayz.vanillap", "Vanilla+", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA882"), "pt", "dayz.vanilla", "Vanilla", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA885"), "pt", "dayz.hardcore", "Hardcore", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA886"), "pt", "dayz.fpp", "FPP", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA887"), "pt", "dayz.tpp", "TPP", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA888"), "pt", "dayz.pvp", "PvP", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA889"), "pt", "dayz.pve", "PvE", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA88A"), "pt", "dayz.solo", "Solo", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA88B"), "pt", "dayz.duo", "Duo", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA88C"), "pt", "dayz.trio", "Trio", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA88D"), "pt", "dayz.fmax", "F-Max", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA88E"), "pt", "dayz.fullmod", "Full Mod", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA88F"), "pt", "dayz.fullpvp", "Full PvP", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA890"), "pt", "dayz.deathmatch", "Deathmatch", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA891"), "pt", "dayz.nobase", "No Base", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA892"), "pt", "dayz.noraid", "No Raid", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA893"), "pt", "dayz.wipe", "Wipe", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA894"), "pt", "dayz.roleplay", "Roleplay", "gameplay_tags"),
+ (UNHEX("08921189C0BE42019E9B6E9E45FEA895"), "pt", "dayz.squad", "Squad", "gameplay_tags");
